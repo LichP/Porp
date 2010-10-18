c56cec49cd5b34a02e5191e61af4c8ea286047a3
Phil <phil@falcon.(none)>
2010-10-18 19:18:51 +0100

    Added find_by_id method

--- porp.rb
+++ porp.rb
@@ -9,11 +9,18 @@
 require 'rubygems'
 require 'redis'
 
+# Make redis available everywhere
 def redis
  $redis ||= Redis.new
 end
 
-ORPNS = 'porp'
+# Namespace for all redis keys
+ns_app = 'porp'
+ns_site = 'mysite'
+ns_deployment = 'dev'
+def ns
+  $ns ||= "#{ns_app}:#{ns_site}:#{ns_deployment}"
+end
 
 class OrpModel
   attr_reader :id
@@ -26,13 +33,23 @@ class OrpModel
     @id.to_s == other.id.to_s
   end
   
+  # Fetch the next available id for new record. This method sets a record key
+  # with this id so effectively creates the record  
   def self.new_id
     klass = self.name.downcase
-    id = redis.incr("#{ORPNS}:#{klass}:uid")
-    redis.set("#{ORPNS}:#{klass}:id:#{id.to_s}", 1)
+    id = redis.incr("#{ns}:#{klass}:uid")
+    redis.set("#{ns}:#{klass}:id:#{id.to_s}:created", 1)
     id
   end
-  
+
+  # Find a record by id
+  def self.find_by_id(id)
+    klass = self.name.downcase
+    if redis.key?("#{ns}:#{klass}:id:#{id.to_s}:created")
+    self.new(id)
+  end
+
+  # Create accessor methods for attributes stored as simple values in the db
   def self.property(name)
     klass = self.name.downcase
     self.class_eval <<-EOCE
@@ -54,13 +71,19 @@ end
 class StockEntity < OrpModel
   property :description
 
+  # Create a new StockEntity record
   def self.create(description)
     new_stock_entity = self.new(self.new_id)
     new_stock_entity.description = description
     new_stock_entity
-  end  
+  end
+  
 end
 
 test = StockEntity.create("Pie")
 
 puts test.description
+puts test.id
+
+test_retrieve = StockEntity.find_by_id(test.id)
+puts test_retrieve.description
