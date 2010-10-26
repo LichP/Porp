35d2c574dc27859baccd31499aee97bef0fa2142
Phil Stewart <phil.stewart@lichp.co.uk>
2010-10-26 19:31:03 +0100

    Unit test stuff; Added tags to sale entities

--- lib/porp.rb
+++ lib/porp.rb
@@ -42,7 +42,7 @@ class Porp
 
   # List all keys in the current namespace
   def self.ns_keys
-    [redis.get(/^#{ns}/)].flatten
+    [redis.keys(/^#{ns}/)].flatten
   end  
 
   # Deletes all keys in the current namespace. This is intended primarily
@@ -135,13 +135,14 @@ class Porp
   class SaleEntity < OrpModel
     property :description, :long_desc
   
-    # Create a new StockEntity record
+    # Create a new SaleEntity record
     def self.create(description)
       new_sale_entity = self.new(self.new_id)
       new_sale_entity.description = description
       new_sale_entity
     end
-  
+
+    # Associates the sale entity with the supplied stock identity   
     def add_stock_entity(stock_id)
       if StockEntity.exists?(stock_id)
         redis.sadd("#{Porp.ns}:saleentity:id:#{id}:stockentities", stock_id)
@@ -150,13 +151,29 @@ class Porp
       end
     end
 
+    # Disassociates the sale entity with the supplied stock identity
     def rem_stock_entity(stock_id)
       redis.srem("#{Porp.ns}:saleentity:id:#{id}:stockentities", stock_id) 
     end
 
-    def sale_entities
-      redis.smembers("#{Porp.ns}:saleentity:id:#{id}:stockentities)")
-    end  
+    # Returns a list of all stock entity ids associated with the sale entity
+    def stock_entities
+      redis.smembers("#{Porp.ns}:saleentity:id:#{id}:stockentities")
+    end
+
+    # Adds a tag 
+    def add_tag(new_tag)
+      new_tag.downcase!  # Also need to strip whitespace
+      redis.sadd("#{Porp.ns}:saleentity:id:#{id}:tags", new_tag)
+      redis.sadd("#{Porp.ns}:saleentities:tags:#{new_tag}", id)
+    end
+
+    # Removes a tag 
+    def remove_tag(dead_tag)
+      dead_tag.downcase!  # Also need to strip whitespace
+      redis.srem("#{Porp.ns}:saleentity:id:#{id}:tags", dead_tag)
+      redis.srem("#{Porp.ns}:saleentities:tags:#{dead_tag}", id)
+    end
   end
 end
   
--- test/stockentity_test.rb
+++ test/stockentity_test.rb
@@ -3,9 +3,12 @@ prepare do
 end
 
 setup do
-
+  Porp.purge_current_namespace!
 end
 
 test "new stock entity should match input data" do
-
+  desc = "Stock entity creation test"
+  new_stke = Porp::StockEntity.new(desc)
+  assert 1 == new_stke.id
+  assert desc == new_stke.description
 end
